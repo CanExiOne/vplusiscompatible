3501e7efb323cff45ab68b7d0d21e42547ae001b
CanExiOne <canexione@gmail.com>
2023-08-02 22:56:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# How to Use
+
+There two ways to run this program.
+
+1. Drag & drop .dll files from mods that you want to modify
+
+2. Provide file names and their path if they are in different directory as arguments in terminal, ex. ``vplusiscompatbile.exe MyMod.dll MyMod2.dll``
+
+It is recommended to first move or copy .dll files from mods to the same directory where you have this program.
+
+## Disclaimer
+
+I do not take responsibility for any issues as explained in License, it's up to you to use this software as it's intended to be used!
+
+### Contact
+
+If the software doesn't work as intended create a new issue.
+
+If you have question you can find me on Discord(@canexione) or send me an email at <canexione@gmail.com>
+
+Windows and your browser might show a pop-up that this software is not safe. It's a safety feature which gets trigerred because it's an unknown file with no certificate, the software is safe and you can check the code yourself here on [Github](https://github.com/canexione/vplusiscompatible)
+
+### How to build
+
+You need python and pyinstaller and then execute this command:
+``pyinstaller --onefile vplusiscompatible.py``
+
+You will find .exe file in the ``dist`` directory.
+
+### License
+
+This software is licensed under [GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/)
---
+++ vplusiscompatible.py
@@ -0,0 +1,94 @@
+import mmap
+import random
+import string
+import sys
+import os.path
+import shutil
+from contextlib import redirect_stdout
+import logging
+
+github = "https://github.com/canexione/vplusiscompatible"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+print("Created by CanExiOne, download this software only from GitHub for safety reasons! " + github)
+
+# Assign files from arguments to list
+files = sys.argv[1:]
+
+logging.info("Files to process: " + str(len(files)))
+
+# Make a file backup
+def make_backup(file):
+      backup_dir = os.getcwd() + "\\backup\\"
+      file_backup = backup_dir + os.path.basename(file) + "_backup"
+
+      try:
+            os.makedirs(backup_dir, exist_ok=True)
+            if(os.path.isfile(file_backup)):
+                  logging.info("A backup file already exists! Replacing file with new backup!")
+                  os.remove(file_backup)
+            else: 
+                  logging.info(f"A copy of file has been made in: {file_backup}")
+
+            shutil.copy(file, file_backup)
+
+      except IOError as ioerror:
+            logging.error("An error has occurred!" + ioerror)
+
+
+# Generate random string with the same size/length as valheim_plus (12)
+# It's random so people can't just add another incompatibility like "valheim_plud"
+def get_random_string():
+      letters = string.ascii_lowercase
+      result = ''.join(random.choice(letters) for i in range(12))
+      logging.info("Random string: " + result)
+      return result
+
+# Read file data and check if it has incompatibility with valheim_plus
+def remove_incompatibility(file):
+      try:
+            with open(file, mode="r+", encoding="utf-8") as file_obj:
+                  with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_WRITE) as mmap_obj:
+                        file_data = mmap_obj.read()
+                        check_incompatibility(file_data, mmap_obj, file)
+      except:
+            logging.error("An error has occurred while reading file or checking for incompatibility!")
+
+# Check for incompatibility and remove if it exists
+def check_incompatibility(file_data, mmap_obj, file):
+      try:
+            if b"valheim_plus" in file_data:
+                        make_backup(file)
+                        try:
+                              file_new_data = file_data.replace(b"valheim_plus", get_random_string().encode())
+                              mmap_obj[:] = file_new_data
+                              mmap_obj.flush()
+                              logging.info("Successfully removed V+ incompatibility in a mod " + os.path.basename(file))
+                        except:
+                              logging.critical(f"Couldn't remove incompatibility in the file! Contact this script developer at: {github}")
+            else:
+                  logging.error("File " + os.path.basename(file) + " doesn't have incompatibility with Valheim Plus!")
+
+      except:
+            logging.critical("An error has occurred!")
+
+for file in files:
+      if(os.path.isfile(file)):
+            logging.info(f"Processing file {file}...")
+            remove_incompatibility(file)
+      else:
+            logging.error(f"File {file} does not exist!")
+
+if len(sys.argv) <= 1:
+      logging.critical("Provide filename as an argument or drag&drop file on the script!")
+
+print("All files processed! Press ENTER to close the program...")
+input()
